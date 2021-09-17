4a4194203954921aca05e7e533576b3a1f8c1dda
Blue <13604413+Bluethefurry@users.noreply.github.com>
2021-09-17 13:12:48 +0200

    add advancedvehicleshop and server.cfg boilerplate

---
+++ [esx_addons]/esx_advancedvehicleshop/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Advanced Vehicle Shop'
+
+Author 'Human Tree92 | Velociti Entertainment'
+
+version 'legacy'
+
+shared_script '@es_extended/imports.lua'
+
+server_scripts {
+	'@async/async.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/*.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/*.lua',
+	'config.lua',
+	'client/utils.lua',
+	'client/main.lua'
+}
+
+dependencies {
+	'es_extended',
+	'esx_dmvschool',
+	'esx_licenseshop'
+}
+
+export 'GeneratePlate'
